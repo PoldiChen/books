739ad22c30338e9637646b3fff19aadb31c85999
PoldiChen <1213386222@qq.com>
2019-03-30 21:06:56 +0800

    add book Effective Java Third Edition.

---
+++ book-effective-java-third-edition/src/main/java/chapter02/rule01/Test.java
@@ -0,0 +1,14 @@
+package chapter02.rule01;
+
+/**
+ * @author poldi.chen
+ * @className Test
+ * @description TODO
+ * @date 2019/3/30 10:08
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
