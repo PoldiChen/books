b413d652018062b7da767794c12db46b4b2f4b5e
PoldiChen <1213386222@qq.com>
2019-06-09 16:47:39 +0800

    add a new book Core Java Volumn 10th Edition.

---
+++ book-core-java-volume/src/main/java/chapter03/Page34.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+/**
+ * @author poldi.chen
+ * @className Page34
+ * @description TODO
+ * @date 2019/6/9 14:47
+ **/
+public class Page34 {
+
+    public static void main(String[] args) {
+        System.out.println(2.0f - 1.1f); // 0.9
+        System.out.println(2.0 - 1.1); // 0.8999999999999999
+    }
+}
---
+++ book-core-java-volume/src/main/java/chapter03/Page37.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+/**
+ * @author poldi.chen
+ * @className Page37
+ * @description TODO
+ * @date 2019/6/9 14:56
+ **/
+public class Page37 {
+
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a); // Error: 可能尚未初始化变量a
+    }
+}
